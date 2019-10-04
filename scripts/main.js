84f9cb7f88306730ef4493f7d32da021b6a57d98
panduwijayasaputra <panduwijayasaputra@gmail.com>
2019-10-05 02:25:19 +0700

    mobile navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,30 @@
-$(document).ready(function () {
-  $('.product-detail-content__information__menu ul li a').click(function (event) {
+$(document).ready(function() {
+  $(".product-detail-content__information__menu ul li a").click(function(
+    event
+  ) {
     event.preventDefault();
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    $($(this).data('target')).addClass('show');
-    $($(this).data('target')).siblings().removeClass('show');
+    $(this)
+      .parent()
+      .addClass("active");
+    $(this)
+      .parent()
+      .siblings()
+      .removeClass("active");
+    $($(this).data("target")).addClass("show");
+    $($(this).data("target"))
+      .siblings()
+      .removeClass("show");
   });
 
-  $('#namaLengkap').focus(function () {
-    console.log($(this).prev());
-    $(this).prev().css({ "color": "#0673ce" });
-  })
+  // $('#namaLengkap').focus(function () {
+  //   console.log($(this).prev());
+  //   $(this).prev().css({ "color": "#0673ce" });
+  // })
+
+  $(".burger").click(function() {
+    console.log("click");
+    $(".mobile").toggleClass("show");
+    $(".mobile__nav").toggleClass("show");
+    $(this).toggleClass("show");
+  });
 });
